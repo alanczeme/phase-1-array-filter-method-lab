7d136f2427450c8b10d4bcc857463e1e59dea1ef
Alan Czemerinski <alanczeme@gmail.com>
2022-01-13 11:08:25 -0500

    commit tree c57964e20f0674e37ac312455aeb97a0ccb1f491

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(oneElement => oneElement.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(drivers, letters) {
+    const lettersLength = letters.length;
+    return drivers.filter((driver) => driver.slice(0,lettersLength) === letters);
+}
+
+function matchName(drivers, nameToMatch) {
+    return drivers.filter((driver) => driver.name === nameToMatch)
+}
